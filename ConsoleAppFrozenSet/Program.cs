cc749e71d7275375d4d35dd940d851217019be7a
Renato Groffe <renato.groffe@yahoo.com.br>
2023-11-02 21:38:39 -0300

    .NET 8 + Console Application + Coleções imutáveis com FrozenSet
    .NET 8 + Console Application + Coleções imutáveis com FrozenSet

---
+++ ConsoleAppFrozenSet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Frozen;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+Console.WriteLine("***** Testes com .NET 8 | Colecoes imutaveis com FrozenSet  *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var campeoesMundiais = new HashSet<string>()
+{
+    "Brasil",
+    "Italia",
+    "Alemanha",
+    "Argentina",
+    "Uruguai",
+    "Franca",
+    "Inglaterra",
+    "Espanha"
+}.ToFrozenSet();
+
+Console.WriteLine();
+Console.WriteLine($"Quantidades de paises que ja venceram Copas: {campeoesMundiais.Count}");
+
+Console.WriteLine();
+Console.WriteLine("JSON com os campeos da Copa do Mundo: ");
+Console.WriteLine(JsonSerializer.Serialize(campeoesMundiais,
+    new JsonSerializerOptions() { WriteIndented = true }));
